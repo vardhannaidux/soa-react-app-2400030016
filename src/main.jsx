ce8058f486cc5c80309a4254c7a8baf0bef1c9ce
vardhan30016 <vardhannaidu17@gmail.com>
2026-09-04 19:47:29 +0530

    updated navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//import './index.css'
+import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
@@ -8,4 +8,3 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
-
